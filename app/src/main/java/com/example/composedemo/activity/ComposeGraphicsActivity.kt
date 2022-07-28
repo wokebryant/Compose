ed9a6304b5ed75fb2a2d7c338b6c70c11f647c48
wokebryant <wokebryant@outlook.com>
2022-07-28 15:19:05 +0800

    feat:自定义绘制(基本图形)

--- app/src/main/java/com/example/composedemo/activity/ComposeGraphicsActivity.kt
+++ app/src/main/java/com/example/composedemo/activity/ComposeGraphicsActivity.kt
@@ -1,14 +1,18 @@
 package com.example.composedemo.activity
 
-import android.util.Log.d
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.withTransform
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composedemo.base.BaseActivity
 
 /**
@@ -21,42 +25,86 @@ class ComposeGraphicsActivity : BaseActivity() {
     @Preview
     @Composable
     override fun ShowPreview() {
-        val LocalColor = compositionLocalOf { Color.Gray }
-
-        Column() {
-            CompositionLocalProvider(
-                LocalContentColor provides Color.Gray,
-            ) {
-
-            }
+        Column(modifier = Modifier.fillMaxSize()) {
+            DrawLine()
+            DrawCircle()
+            DrawRect()
         }
     }
 
     /**
-     *  自定义一个主题
+     * 绘制线段
      */
     @Composable
-    private fun CustomColorTheme(isDark: Boolean, content: @Composable () -> Unit) {
-        val BLUE = Color(0xFF0000FF)
-        val RED = Color(0xFFDC143C)
-        val colors = if (isDark) {
-            darkColors(
-                primary = BLUE
-            )
-        } else {
-            lightColors(
-                primary = RED
-            )
+    fun DrawLine() {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(color = Color.Gray)
+        ) {
+           val canvasWidth = size.width
+           val canvasHeight = size.height
+            val a = size / 2F
+
+           drawLine(
+               start = Offset(x = canvasWidth, y = 0f),
+               end = Offset(x = 0f, y = canvasHeight),
+               color = Color.Green,
+               strokeWidth = 4.0f
+           )
         }
-
-
-        MaterialTheme(
-            colors = colors,
-            content = content
-        )
     }
 
+    /**
+     * 绘制圆
+     */
+    @Composable
+    fun DrawCircle() {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(color = Color.Gray)
+        ) {
+            val canvasWidth = size.width
+            val canvasHeight = size.height
 
+            drawCircle(
+                color = Color.Blue,
+                center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
+                radius = size.minDimension / 3
+            )
+        }
+    }
 
+    /**
+     *  绘制矩形
+     *  可变属性
+     */
+    @Composable
+    fun DrawRect() {
+        Canvas(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp)
+        ) {
+            val canvasSize = size
+            val canvasWidth = size.width
+            val canvasHeight = size.height
+
+            withTransform({
+                translate(left = canvasWidth / 2)
+                rotate(degrees = 45f)
+            }) {
+                drawRect(
+                    color = Color.Cyan,
+                    topLeft = Offset(x = canvasWidth / 5f, y = canvasHeight / 3f),
+//                    size = Size(width = 100f, height = 400f)
+                    size = canvasSize / 3f
+                )
+            }
+        }
+    }
 
 }
